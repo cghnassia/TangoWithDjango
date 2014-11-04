c84a27925250ff6e3203903db3f23b489a6d82c0
Clément GHNASSIA <clement.ghnassia@gmail.com>
2014-11-03 21:17:18 -0800

    fix static directive

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -86,12 +86,12 @@ LOGIN_URL = '/rango/login/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
